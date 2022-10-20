ac33c36604441bee4e2bfece15ee294087de84e0
jessica248 <110992337+jessica248@users.noreply.github.com>
2022-10-20 09:41:20 -0300

    20/10

--- funcao.js
+++ funcao.js
@@ -1,16 +1,35 @@
-var P = 0
+function peca() {
 
-function Peca()
-    {
-        P=0
-        if (Peca.value=="--")
-        {
-            P=0
-        }
-        if (Peca.value=="Camisas")
-        {
-            P=34
-        }
-        if ()
+    //var preco = 0
+    //var quantidade = document.getElementById("quantidade").value
+
+    var teste = document.getElementById("id_peca").value
+    alert(`teste: ${teste}`)
+/*
+    if (document.getElementById("id_peca").value == "") {
+        preco = 0
     }
+    if (document.getElementById("id_peca").value == "Camisas") {
+        preco = 34
+    }
+    if (document.getElementById("id_peca").value == "Calças") {
+        preco = 45
+    }
+    if (document.getElementById("id_peca").value == "Bermudas") {
+        preco = 38
+    }
+    if (document.getElementById("id_peca") == "Saias") {
+        preco = 40
+    }
+    if (document.getElementById("id_peca") == "Shorts") {
+        preco = 25
+    }
+
+    var preco_final = preco * quantidade
+
+    document.getElementById("preco_final").value = preco_final
+*/
+}
+
+
 
