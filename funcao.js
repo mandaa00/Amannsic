96d9172d32c85be38645ed181e7754fc19d7854a
jessica248 <110992337+jessica248@users.noreply.github.com>
2022-10-20 08:08:25 -0300

    teste funcao

---
+++ funcao.js
@@ -0,0 +1,16 @@
+var P = 0
+
+function Peca()
+    {
+        P=0
+        if (Peca.value=="--")
+        {
+            P=0
+        }
+        if (Peca.value=="Camisas")
+        {
+            P=34
+        }
+        if ()
+    }
+
