02d5d5999cb4c6a6d8302645274139d4d72d3171
jessica248 <110992337+jessica248@users.noreply.github.com>
2022-11-01 07:37:11 -0300

    ggg
    g

--- cadastrar.php
+++ cadastrar.php
@@ -1,5 +1,31 @@
 <?php
+<<<<<<< Updated upstream
 include("conecta.php");
+=======
+include ("config.php");
+$nome=$_POST["nome"];
+$telefone=$_POST["telefone"];
+$email=$_POST["email"];
+$senha=$_POST["senha"];
+
+$comando = $pdo->prepare("INSERT INTO cadastro(nome, telefone, email, senha) VALUES(:nome,:telefone,:email,:senha) ");
+
+    $comando->bindValue(":nome",$nome);
+    $comando->bindValue(":telefone",$telefone);
+    $comando->bindValue(":email",$email);
+    $comando->bindValue(":senha",$senha);
+   
+    $comando->execute();
+    
+    header("Location:catalogo.html");
+    unset($comando);
+    unset($pdo);
+?>
+   
+  
+
+
+>>>>>>> Stashed changes
 
 $nome = $_POST["nome"];
 $telefone = $_POST["telefone"];
