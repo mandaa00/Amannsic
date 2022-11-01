c16aea03c060e604e223dfea5a5b07992a92550f
jessica248 <110992337+jessica248@users.noreply.github.com>
2022-11-01 08:06:21 -0300

    11
    11

--- cadastrar.php
+++ cadastrar.php
@@ -1,32 +1,5 @@
 <?php
-<<<<<<< Updated upstream
 include("conecta.php");
-=======
-include ("config.php");
-$nome=$_POST["nome"];
-$telefone=$_POST["telefone"];
-$email=$_POST["email"];
-$senha=$_POST["senha"];
-
-$comando = $pdo->prepare("INSERT INTO cadastro(nome, telefone, email, senha) VALUES(:nome,:telefone,:email,:senha) ");
-
-    $comando->bindValue(":nome",$nome);
-    $comando->bindValue(":telefone",$telefone);
-    $comando->bindValue(":email",$email);
-    $comando->bindValue(":senha",$senha);
-   
-    $comando->execute();
-    
-    header("Location:catalogo.html");
-    unset($comando);
-    unset($pdo);
-?>
-   
-  
-
-
->>>>>>> Stashed changes
-
 $nome = $_POST["nome"];
 $telefone = $_POST["telefone"];
 $email = $_POST["email"];
@@ -39,8 +12,9 @@ $comando->bindValue(":telefone",$telefone);
 $comando->bindValue(":email",$email);
 $comando->bindValue(":senha",$senha);
 
-$resultado = $comando->execute();
-?>
-    <script>
-      window.open("catalogo.html","_self");
-    </script>
+$comando->execute();
+
+header("Location:catalogo.html");
+
+unset($comando);
+unset($pdo);
